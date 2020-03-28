49c58e227ef5c94c55083b252290451d4128c5cb
Kennedy Rose <kennedyianrose@gmail.com>
2020-03-28 10:11:26 -0500

    Wildcards in export paths

--- packages/omni/src/export-files.js
+++ packages/omni/src/export-files.js
@@ -1,4 +1,4 @@
-import { join, extname } from 'path'
+import { join, extname, basename } from 'path'
 import replaceExt from 'replace-ext'
 import { outputFile } from 'fs-extra'
 
@@ -11,14 +11,26 @@ export default function exportFiles() {
 				const dirs = block.directives
 				if (!dirs.export) continue
 
-				console.log(`data`, data)
-				let blockPath = dirs.export[0]
+				// Get original .omni file name
+				let extension = extname(data.path)
+				let originalName = basename(data.path, extension)
 
+				// Get code block export path from export directive argument
+				let blockPath = dirs.export[0]
+				if (blockPath) {
+					blockPath = blockPath.replace(/\*/g, originalName)
+					if(blockPath.charAt(blockPath.length - 1) === `/`){
+						blockPath = join(blockPath, originalName)
+					}
+				}
+
+				// Create final output path
 				let outputPath = join(omni.config.output, blockPath || data.path)
 				let ext = extname(outputPath)
 				if (!ext) {
 					outputPath = replaceExt(outputPath, `.${block.type}`)
 				}
+
 				promises.push(outputFile(outputPath, block.code))
 			}
 
